122c1c38f0df4128b9f5c8971b9d17fc22fde674
Devin Medeiros <devin_medeiros@hotmail.com>
2014-05-02 14:54:48 -1000

    Initial Commit

---
+++ Overdue Task List Assignment/CKViewController.m
@@ -0,0 +1,29 @@
+//
+//  CKViewController.m
+//  Overdue Task List Assignment
+//
+//  Created by Devin Medeiros on 5/2/14.
+//  Copyright (c) 2014 Cutie Killer Apps. All rights reserved.
+//
+
+#import "CKViewController.h"
+
+@interface CKViewController ()
+
+@end
+
+@implementation CKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Overdue Task List Assignment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Overdue Task List Assignment
+//
+//  Created by Devin Medeiros on 5/2/14.
+//  Copyright (c) 2014 Cutie Killer Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CKAppDelegate class]));
+    }
+}
